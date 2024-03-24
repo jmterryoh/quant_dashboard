ec0ea9ec5eb1ce7f520180f583081ac798fe137b
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-03-24 19:08:10 +0900

    Add files via upload

---
+++ pages/sidebar.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from util import session as ss
+
+def authenticated_menu():
+    # Show a navigation menu for authenticated users
+    st.sidebar.page_link("pages/page_list_searched.py", label="조건검색")
+    st.sidebar.page_link("pages/page_chart_analysis_basic.py", label="차트분석")
+    st.sidebar.divider()
+    logout = st.sidebar.button(label="Logout")
+    if logout:
+        ss.clear_session_logout()
+    #if st.session_state.role in ["admin"]:
+    #    st.sidebar.page_link("pages/page2.py", label="차트분석")
+
+def menu_with_redirect():
+    # Redirect users to the main page if not logged in, otherwise continue to
+    # render the navigation menu
+#    if "role" not in st.session_state:
+#        st.switch_page("login.py")
+    authenticated_menu()
