60f8e49c0a4d62c763465f09daafbb4a37d913ab
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-03-26 22:40:08 +0900

    Update page_dashboard_interest.py

--- pages/page_dashboard_interest.py
+++ pages/page_dashboard_interest.py
@@ -118,7 +118,7 @@ def display_page():
         with col1:
             indicators_params_dy = {}
             indicators_params_dy = get_stock_indicators(uidx=1, market=market, code=code)
-            display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_dy, cycle="일봉", period="5y", interval="1d", time_minspacing=15, height=350)
+            display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_dy, cycle="일봉", period="3y", interval="1d", time_minspacing=15, height=350)
 
         with col2:
             if 'screen_width' in st.session_state:
