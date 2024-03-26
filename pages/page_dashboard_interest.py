75b60da04b4736e3f38cd48fb12acc4d996378bb
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-03-26 23:07:52 +0900

    Update page_dashboard_interest.py

--- pages/page_dashboard_interest.py
+++ pages/page_dashboard_interest.py
@@ -121,15 +121,15 @@ def display_page():
             display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_dy, cycle="일봉_확대", period="3y", interval="1d", time_minspacing=15, height=350)
 
         with col2:
-            if 'screen_width' in st.session_state:
-                width = st.session_state['screen_width']
-                if width >= 800:
-                    #indicators_params_wk = {}
-                    #indicators_params_dy = get_stock_indicators(uidx=1, market=market, code=code)
-                    #display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_wk, cycle="주봉", period="10y", interval="1wk", height=350)
-                    indicators_params_dy = {}
-                    indicators_params_dy = get_stock_indicators(uidx=1, market=market, code=code)
-                    display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_dy, cycle="일봉_와이드", period="3y", interval="1d", time_minspacing=3, height=350)
+            #if 'screen_width' in st.session_state:
+            #    width = st.session_state['screen_width']
+            #    if width >= 800:
+            #indicators_params_wk = {}
+            #indicators_params_dy = get_stock_indicators(uidx=1, market=market, code=code)
+            #display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_wk, cycle="주봉", period="10y", interval="1wk", height=350)
+            indicators_params_dy = {}
+            indicators_params_dy = get_stock_indicators(uidx=1, market=market, code=code)
+            display_stock_charts(market=market, name=name, code=code, indicators_params=indicators_params_dy, cycle="일봉_와이드", period="3y", interval="1d", time_minspacing=3, height=350)
 
 
 button_refresh = None
