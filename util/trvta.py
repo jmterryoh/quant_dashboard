417bce4d16b284967d7dae31dd4a4bb2d1fcf8a7
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-04-05 12:40:45 +0900

    Add files via upload

---
+++ util/trvta.py
@@ -0,0 +1,15 @@
+from tradingview_ta import *
+
+def get_tradingview_ta(stock_codes, interval=Interval.INTERVAL_1_DAY):
+    recommendation_list = []
+    krx_symbols = ['KRX:' + symbol for symbol in stock_codes]
+    if krx_symbols:
+        analysis = get_multiple_analysis(screener="korea", interval=interval, symbols=krx_symbols)
+        for symbol in analysis:
+            if analysis[symbol]:
+                recommendation = analysis[symbol].summary['RECOMMENDATION']
+                recommendation = recommendation.replace('STRONG_BUY','강력매수').replace('STRONG_SELL','강력매도').replace('BUY','매수').replace('SELL','매도').replace('NEUTRAL','중립')
+                indicator = f"+{analysis[symbol].summary['BUY']},-{analysis[symbol].summary['SELL']},({analysis[symbol].summary['NEUTRAL']})"
+                recommendation_list.append({"code":symbol.split(':')[1], "recommendation":recommendation, "indicator":indicator})
+
+    return recommendation_list
