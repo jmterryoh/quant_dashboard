194fe9afda211a60193d62d593049f2f5378088b
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-04-07 21:47:42 +0900

    Update trvta.py

--- util/trvta.py
+++ util/trvta.py
@@ -1,3 +1,4 @@
+# https://python-tradingview-ta.readthedocs.io/en/latest/usage.html#instantiating-ta-handler
 from tradingview_ta import *
 
 def get_tradingview_ta(stock_codes, interval=Interval.INTERVAL_1_DAY):
@@ -7,9 +8,9 @@ def get_tradingview_ta(stock_codes, interval=Interval.INTERVAL_1_DAY):
         analysis = get_multiple_analysis(screener="korea", interval=interval, symbols=krx_symbols)
         for symbol in analysis:
             if analysis[symbol]:
-                recommendation = analysis[symbol].summary['RECOMMENDATION']
+                recommendation = analysis[symbol].oscillators['RECOMMENDATION']
                 recommendation = recommendation.replace('STRONG_BUY','강력매수').replace('STRONG_SELL','강력매도').replace('BUY','매수').replace('SELL','매도').replace('NEUTRAL','중립')
-                indicator = f"+{analysis[symbol].summary['BUY']},-{analysis[symbol].summary['SELL']},({analysis[symbol].summary['NEUTRAL']})"
+                indicator = f"+{analysis[symbol].oscillators['BUY']},-{analysis[symbol].oscillators['SELL']},({analysis[symbol].oscillators['NEUTRAL']})"
                 recommendation_list.append({"code":symbol.split(':')[1], "recommendation":recommendation, "indicator":indicator})
 
     return recommendation_list
