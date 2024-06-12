ae42703fce89ab220db5ce95a4e77042335d2811
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-06-12 20:51:26 +0900

    Add files via upload

--- pages/page_chart_analysis_vwap.py
+++ pages/page_chart_analysis_vwap.py
@@ -334,7 +334,7 @@ def main():
                         data_count = days_difference * 8 * 3 # 1시간 기준 1시간: 1개, 7.5시간: 8
                         interval = Interval.in_1_hour
                     elif selected_minutes == '1일':
-                        data_count = days_difference * 10  # 1일 기준, 1개
+                        data_count = days_difference * 5  # 1일 기준, 1개
                         interval = Interval.in_daily
 
  
