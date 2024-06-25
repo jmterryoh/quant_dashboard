8679f11361f441017314c3e00b0307999705e429
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-06-25 16:33:07 +0900

    Add files via upload

--- util/zigzag.py
+++ util/zigzag.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import pytz
+from datetime import datetime, timedelta
+import FinanceDataReader as fdr
+
 
 # Zig-Zag
 def calculate_zigzag(base_prices):
@@ -38,9 +42,9 @@ def calculate_zigzag(base_prices):
     return pivot_points
 
 
-def get_zigzag(df):
+def get_zigzag(df, base_price="Close"):
     # Close values 
-    data_df = df['close'].values
+    data_df = df[f"{base_price}"].values
     dates = df.index
 
     zigzag_pivots = calculate_zigzag(base_prices=data_df)
@@ -50,15 +54,15 @@ def get_zigzag(df):
 
     # 마지막 pivot 값 추가
     # 마지막 time, value 값 추가
-    zigzag_lines_data.append({"time":df.index[-1], "value":df.iloc[-1]['close'], "pivot": "last"})
+    zigzag_lines_data.append({"time":df.index[-1], "value":df.iloc[-1][f'{base_price}'], "pivot": "last"})
 
     return pd.DataFrame(zigzag_lines_data)
 
 
-def get_zigzag_lines(dataframe, window_size=10, std_threshold=0.01):
+def get_zigzag_lines(dataframe, base_price="Close", window_size=10, std_threshold=0.01):
 
     # Close values 
-    stock_prices = dataframe['close'].values
+    stock_prices = dataframe[f"{base_price}"].values
     dates = dataframe.index
 
     # Set the number of periods for calculating the standard deviation
@@ -78,41 +82,69 @@ def get_zigzag_lines(dataframe, window_size=10, std_threshold=0.01):
     # 마지막 pivot 값 추가
     #zigzag_lines_data.append({"time": dates[pivot[2]], "value": pivot[3]})
     # 마지막 time, value 값 추가
-    zigzag_lines_data.append({"time":dataframe.index[-1], "value":dataframe.iloc[-1]['close']})
+    zigzag_lines_data.append({"time":dataframe.index[-1], "value":dataframe.iloc[-1][f"{base_price}"]})
 
     return pd.DataFrame(zigzag_lines_data)
 
 
-# Input : df = Dataframe(['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
-#         anchor_date = anchord datetime
-#         multiplier1 = vwap band 1
-#         multiplier2 = vwap band 2 
-# Output : Dataframe(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'VWAP', 'STD+1', 'STD-1', 'STD+2', 'STD-2'])
-def calculate_vwap_bands(df, anchor_date, vwap_name, multiplier1, multiplier2, vwap_color):    
-    # Use .loc[] to avoid setting values on a slice of DataFrame
-    df1 = df.loc[df.index >= anchor_date].copy()
+# 직전 valley 일자를 찾는 함수
+def find_recent_valley_before_date(zigzag_df, input_date, days_before=90):
+    # input_date 형식 변환
+    target_date = datetime.strptime(str(input_date), '%Y%m%d').date()
+    
+    # 입력된 날짜의 value 찾기
+    target_value = None
+    zigzag_df = zigzag_df.iloc[::-1]
+    for index, row in zigzag_df.iterrows():
+        entry_date = row['time'].date()
+        if entry_date == target_date and row['pivot'] == 'valley':
+            target_value = row['value']
+            break
+    
+    # 해당 valley 값의 -1% 아래값
+    if target_value is None:
+        return None    
+    target_value = int(target_value * 0.99)
 
-    # VWAP Formula : ACC (Close * Volume) / ACC (Volume)
-    data = pd.DataFrame({
-        'close': df1['close'],  # Close value base
-        'volume': df1['volume'],
-        'TP': (df1['close'] * df1['volume']),
-        vwap_name: (df1['close'] * df1['volume']).cumsum() / df1['volume'].cumsum()
-    })
-    df1[vwap_name] = data[vwap_name]
+    # 오늘 날짜 설정 (한국 시간대)
+    three_months_ago = target_date - timedelta(days=days_before)
 
-    if multiplier1 is not None or multiplier2 is not None:
-        # data['TPP'] = (df1['Close'] * df1['Close'] * df1['Volume']).cumsum() / df1['Volume'].cumsum()
-        # STD Formula : sqrt( (ACC(Close X Close * Volume) / ACC(Volume)) - VWAP * VWAP )
-        # multiple 1: 1.28, mutiple 2: 2.01, multiple 3: 2.51
-        data['TPP'] = (data['close'] * data['TP']).cumsum() / data['volume'].cumsum() # Volume!! 
-        data['VW2'] = data[vwap_name] * data[vwap_name]
-        data['STD'] = (data['TPP'] - data['VW2']) ** 0.5
-        if multiplier1 is not None:
-            df1.loc[:, 'STD+1'] = data[vwap_name] + data['STD'] * multiplier1
-            df1.loc[:, 'STD-1'] = data[vwap_name] - data['STD'] * multiplier1
-        if multiplier2 is not None:
-            df1.loc[:, 'STD+2'] = data[vwap_name] + data['STD'] * multiplier2
-            df1.loc[:, 'STD-2'] = data[vwap_name] - data['STD'] * multiplier2
+    # 입력된 날짜 이전의 가장 최근 valley의 날짜 찾기
+    recent_valley_date = None
+    for index, row in zigzag_df.iterrows():
+        entry_date = row['time'].date()
+        if three_months_ago <= entry_date < target_date and row['pivot'] == 'valley' and row['value'] < target_value:
+            recent_valley_date = entry_date
+            break
+    
+    if recent_valley_date is None:
+        return None
 
-    return df1, vwap_color
+    return recent_valley_date.strftime('%Y%m%d')
+
+
+def get_previous_valley_date(stock_code, vdt, base_price="Close", days_before=90):
+
+    try:
+        # vdt
+        #vdt_date = pytz.timezone('Asia/Seoul').localize(datetime.strptime(vdt, "%Y%m%d"))
+        today = datetime.now(pytz.timezone('Asia/Seoul'))
+        # vdt 이전 90일
+        end_date = today
+        start_date = end_date - timedelta(days=days_before)
+        end_date = end_date.replace(tzinfo=None)
+        start_date = start_date.replace(tzinfo=None)
+
+        price_1day_df = fdr.DataReader(stock_code, start_date, end_date)
+        if price_1day_df is not None and not price_1day_df.empty:
+            zigzag_df = get_zigzag(price_1day_df, base_price=base_price)
+            if zigzag_df is not None and not zigzag_df.empty:
+                previous_vdt = find_recent_valley_before_date(zigzag_df, vdt, days_before=days_before)
+                if previous_vdt is None:
+                    return vdt, price_1day_df
+                return previous_vdt, price_1day_df
+
+    except Exception as e:
+        print(f"Error in get_previous_valley_date: {str(e)}")
+
+    return None, None
