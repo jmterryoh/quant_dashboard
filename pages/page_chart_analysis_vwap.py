32aa4f4bad6afca36eb5f71946c7a3d0f81f8354
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-07-04 23:17:47 +0900

    Add files via upload

--- pages/page_chart_analysis_vwap.py
+++ pages/page_chart_analysis_vwap.py
@@ -668,8 +668,8 @@ def main():
             vwap_high2_dataframe = calculate_vwap_only(df=price_pvdt_df, vwap_name="vwap", price_base="close", volume_base="volume")
             vwap_high2_dataframe = vwap_high2_dataframe.reset_index()
 
-            st.text(price_pvdt_df)
-            print(pvdt_string_datetime, i10dt_string_datetime, price_high_index)
+            #st.text(price_pvdt_df)
+            #print(pvdt_string_datetime, i10dt_string_datetime, price_high_index)
 
             # vwap_highest_dataframe 장대양봉일(i10dt) 이후 최고점
             i10dt_string_datetime = datetime.strptime(i10dt, "%Y%m%d")
@@ -681,7 +681,7 @@ def main():
             vwap_highest_dataframe = calculate_vwap_only(df=price_pvdt_df, vwap_name="vwap", price_base="close", volume_base="volume")
             vwap_highest_dataframe = vwap_highest_dataframe.reset_index()
 
-            print(pvdt_string_datetime, i10dt_string_datetime, price_high_index)
+            #print(pvdt_string_datetime, i10dt_string_datetime, price_high_index)
             #print(vwap_high2_dataframe)
 
 
