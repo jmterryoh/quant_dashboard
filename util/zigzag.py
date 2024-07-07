5fce3b53824446dd394dcf52a70056ca12ea9c18
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-07-07 18:24:09 +0900

    Add files via upload

--- util/zigzag.py
+++ util/zigzag.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytz
 from datetime import datetime, timedelta
-import FinanceDataReader as fdr
+from hts import TV_rest_api as tv
 
 
 # Zig-Zag
@@ -88,54 +88,50 @@ def get_zigzag_lines(dataframe, base_price="Close", window_size=10, std_threshol
 
 
 # 직전 valley 일자를 찾는 함수
-def find_recent_valley_before_date(zigzag_df, input_date, days_before=90):
+def find_recent_valley_before_date(zigzag_df, input_date, days_before=30):
     # input_date 형식 변환
-    target_date = datetime.strptime(str(input_date), '%Y%m%d').date()
+    target_date = datetime.strptime(str(input_date), '%Y%m%d')
     
-    # 입력된 날짜의 value 찾기
-    target_value = None
-    zigzag_df = zigzag_df.iloc[::-1]
-    for index, row in zigzag_df.iterrows():
-        entry_date = row['time'].date()
-        if entry_date == target_date and row['pivot'] == 'valley':
-            target_value = row['value']
-            break
+    # # 입력된 날짜의 value 찾기
+    # target_value = None
+    # zigzag_df = zigzag_df.iloc[::-1]
+    # for index, row in zigzag_df.iterrows():
+    #     entry_date = row['time'].date()
+    #     if entry_date == target_date and row['pivot'] == 'valley':
+    #         target_value = row['value']
+    #         break
     
-    # 해당 valley 값의 -1% 아래값
-    if target_value is None:
-        return None    
-    target_value = int(target_value * 0.99)
+    # # 해당 valley 값의 -1% 아래값
+    # if target_value is None:
+    #     return None    
+    # target_value = int(target_value * 0.99)
 
-    # 오늘 날짜 설정 (한국 시간대)
-    three_months_ago = target_date - timedelta(days=days_before)
+    # 날짜 설정 (한국 시간대)
+    one_month_ago = target_date - timedelta(days=days_before)
+    zigzag_df = zigzag_df.iloc[::-1]
 
     # 입력된 날짜 이전의 가장 최근 valley의 날짜 찾기
     recent_valley_date = None
     for index, row in zigzag_df.iterrows():
-        entry_date = row['time'].date()
-        if three_months_ago <= entry_date < target_date and row['pivot'] == 'valley' and row['value'] < target_value:
+        entry_date = row['time']
+        #print(entry_date, row['pivot'], target_date)
+        if one_month_ago <= entry_date < target_date and row['pivot'] == 'valley':# and row['value'] < target_value:
+            #print("valley", entry_date)
             recent_valley_date = entry_date
             break
     
     if recent_valley_date is None:
         return None
 
-    return recent_valley_date.strftime('%Y%m%d')
+    return recent_valley_date.strftime('%Y%m%d%H%M%S')
 
 
-def get_previous_valley_date(stock_code, vdt, base_price="Close", days_before=90):
+def get_previous_valley_datetime(stock_code, stock_name, vdt, base_price="close", days_before=30):
 
     try:
         # vdt
         #vdt_date = pytz.timezone('Asia/Seoul').localize(datetime.strptime(vdt, "%Y%m%d"))
-        today = datetime.now(pytz.timezone('Asia/Seoul'))
-        # vdt 이전 90일
-        end_date = today
-        start_date = end_date - timedelta(days=days_before)
-        end_date = end_date.replace(tzinfo=None)
-        start_date = start_date.replace(tzinfo=None)
-
-        price_1day_df = fdr.DataReader(stock_code, start_date, end_date)
+        price_1day_df = tv.get_tvdata_from_vdt_days_before(stock_code=stock_code, stock_name=stock_name, selected_minutes="30분", vdt=vdt, days_before=days_before)
         if price_1day_df is not None and not price_1day_df.empty:
             zigzag_df = get_zigzag(price_1day_df, base_price=base_price)
             if zigzag_df is not None and not zigzag_df.empty:
