dfa3d1304f3555811bc6abf3bb0cd26b43f49b15
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-07-08 22:51:32 +0900

    Add files via upload

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -3,6 +3,9 @@ from util import session as ss
 
 def authenticated_menu():
     # Show a navigation menu for authenticated users
+    st.sidebar.page_link("pages/page_algo_list_searched.py", label="자동관심")
+    st.sidebar.page_link("pages/page_chart_analysis_vwap.py", label="단타분석")
+    st.sidebar.divider()
     st.sidebar.page_link("pages/page_dashboard_interest.py", label="관심종목")
     #st.sidebar.page_link("pages/page_dashboard_owned.py", label="보유종목")
     st.sidebar.divider()
@@ -10,9 +13,6 @@ def authenticated_menu():
     st.sidebar.page_link("pages/page_list_interest.py", label="관심추가")
     st.sidebar.page_link("pages/page_chart_analysis_basic.py", label="차트분석")
     #st.sidebar.page_link("pages/page_list_owned.py", label="거래등록")
-    st.sidebar.divider()
-    st.sidebar.page_link("pages/page_algo_list_searched.py", label="자동관심")
-    st.sidebar.page_link("pages/page_chart_analysis_vwap.py", label="단타분석")
     st.sidebar.divider()
     logout = st.sidebar.button(label="Logout")
     if logout:
