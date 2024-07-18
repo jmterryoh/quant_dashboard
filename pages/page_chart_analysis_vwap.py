dfc376dbb07f87b239d35324e56eb9e7c8530f9b
jmterryoh <41352213+jmterryoh@users.noreply.github.com>
2024-07-18 11:32:21 +0900

    Add files via upload

--- pages/page_chart_analysis_vwap.py
+++ pages/page_chart_analysis_vwap.py
@@ -137,7 +137,7 @@ def get_tvdata_from_vdt(stock_code, stock_name, selected_minutes, vdt, days_more
     vdt_one_week_ago = vdt_date - timedelta(days=0)
     days_difference = (today - vdt_one_week_ago).days + days_more
 
-    interval = Interval.in_1_minute
+    interval = Interval.in_5_minute
     if selected_minutes:
         if selected_minutes == '1분':
             data_count = days_difference * 381  # 1분봉 기준 1시간: 60개, 7.5시간: 450
@@ -575,7 +575,7 @@ def main():
                     today = datetime.now(pytz.timezone('Asia/Seoul'))
                     vdt_one_week_ago = vdt_date - timedelta(days=0)
                     days_difference = (today - vdt_one_week_ago).days
-                    selected_minutes = st.selectbox('분봉 선택', index=2, options=['1분','3분','5분','15분','30분','1시간','1일'], key="minutes")
+                    selected_minutes = st.selectbox('분봉 선택', index=1, options=['1분','3분','5분','15분','30분','1시간','1일'], key="minutes")
                     if selected_minutes:
                         if selected_minutes == '1분':
                             data_count = days_difference * 450  # 1분봉 기준 1시간: 60개, 7.5시간: 450
